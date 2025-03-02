3e089edb8c794f44100a057efde06f3f388b33c8
Robert Robi <stile351@gmail.com>
2025-03-02 18:36:47 +0100

    minor change

--- src/nats/subjects/subjects.js
+++ src/nats/subjects/subjects.js
@@ -2,6 +2,6 @@ module.exports = {
   Subjects: Object.freeze({
     SubscriptionCreated: "subscription:created",
     SubscriptionUpdated: "subscription:updated",
-    PaymentComplited: "payment:complited",
+    PaymentCompleted: "payment:completed",
   }),
 };
