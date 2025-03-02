a247ddf1707a9ce44e08420feb20507378fe8ab2
Robert Robi <stile351@gmail.com>
2025-03-02 21:19:34 +0100

    added SubscriptionCancelled subject

--- src/nats/subjects/subjects.js
+++ src/nats/subjects/subjects.js
@@ -2,6 +2,7 @@ module.exports = {
   Subjects: Object.freeze({
     SubscriptionCreated: "subscription:created",
     SubscriptionUpdated: "subscription:updated",
+    SubscriptionCancelled: "subscription:cancelled",
     PaymentCompleted: "payment:completed",
     PaymentExpirationCompleted: "payment-expiration:completed",
   }),
